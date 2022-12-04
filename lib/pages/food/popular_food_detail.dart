43c6edf76ee6a841e0a2ea2ca72401535f29a324
Mehedi <mehedi@augnitive.com>
2022-12-04 16:44:47 +0600

    bottom nav bar layout designed

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimension.dart';
 
 import '../../widgets/app_column.dart';
@@ -11,6 +12,7 @@ class PopularFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           Positioned(
@@ -78,10 +80,59 @@ class PopularFoodDetail extends StatelessWidget {
           right: Dimensions.width20,
         ),
         decoration: BoxDecoration(
+          color: AppColors.buttonBackgroundColor,
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(Dimensions.radius20 * 2),
             topRight: Radius.circular(Dimensions.radius20 * 2),
           ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: Dimensions.height20,
+                bottom: Dimensions.height20,
+                right: Dimensions.width20,
+                left: Dimensions.width20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  const Icon(
+                    Icons.remove,
+                    color: AppColors.signColor,
+                  ),
+                  SizedBox(width: Dimensions.width10 / 2),
+                  BigText(text: '0'),
+                  SizedBox(width: Dimensions.width10 / 2),
+                  const Icon(
+                    Icons.add,
+                    color: AppColors.signColor,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                top: Dimensions.height20,
+                right: Dimensions.width20,
+                bottom: Dimensions.height20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: AppColors.mainColor,
+              ),
+              child: const BigText(
+                text: '\$10 | Add to cart',
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
