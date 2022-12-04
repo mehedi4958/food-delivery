18d96aa51be027cc72b02a1f48d7b5228ca4f727
Mehedi <mehedi@augnitive.com>
2022-12-04 21:12:51 +0600

    products model created

---
+++ lib/models/products_model.dart
@@ -0,0 +1,90 @@
+class Product {
+  Product({
+    this.totalSize,
+    this.typeId,
+    this.offset,
+    this.products,
+  });
+
+  int? totalSize;
+  int? typeId;
+  int? offset;
+  List<Products>? products;
+
+  Product.fromJson(Map<String, dynamic> json) {
+    totalSize = json['total_size'];
+    typeId = json['type_id'];
+    offset = json['offset'];
+    if (json['products'] != null) {
+      products = <Products>[];
+      json['products'].forEach((e) {
+        products!.add(Products.fromJson(e));
+      });
+    }
+  }
+
+  // Map<String, dynamic> toJson() {
+  //   final Map<String, dynamic> data = <String, dynamic>{};
+  //   data['total_size'] = totalSize;
+  //   data['type_id'] = typeId;
+  //   data['offset'] = offset;
+  //   if (products != null) {
+  //     data['products'] = products!.map((e) => e.toJson()).toList();
+  //   }
+  //   return data;
+  // }
+}
+
+class Products {
+  Products({
+    this.id,
+    this.name,
+    this.description,
+    this.price,
+    this.stars,
+    this.img,
+    this.location,
+    this.createdAt,
+    this.updatedAt,
+    this.typeId,
+  });
+
+  int? id;
+  String? name;
+  String? description;
+  int? price;
+  int? stars;
+  String? img;
+  String? location;
+  String? createdAt;
+  String? updatedAt;
+  String? typeId;
+
+  Products.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    price = json['price'];
+    stars = json['stars'];
+    img = json['img'];
+    location = json['location'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    typeId = json['type_id'];
+  }
+
+  // Map<String, dynamic> toJson() {
+  //   final Map<String, dynamic> data = <String, dynamic>{};
+  //   data['id'] = id;
+  //   data['name'] = name;
+  //   data['description'] = description;
+  //   data['price'] = price;
+  //   data['stars'] = stars;
+  //   data['img'] = img;
+  //   data['location'] = location;
+  //   data['created_at'] = createdAt;
+  //   data['updated_at'] = updatedAt;
+  //   data['type_id'] = typeId;
+  //   return data;
+  // }
+}
