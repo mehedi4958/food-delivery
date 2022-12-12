f814f77e5ed79251687a9333952068ecc66e7bab
Mehedi <mehedi@augnitive.com>
2022-12-12 08:00:19 +0600

    changes made for test purpose

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/dimension.dart';
 import 'package:food_delivery/widgets/small_text.dart';
+import 'package:get/get.dart';
 
+import '../../controllers/popular_product_controller.dart';
+import '../../controllers/recommended_product_controller.dart';
 import '../../utils/colors.dart';
 import '../../widgets/big_text.dart';
 import 'food_page_body.dart';
@@ -14,10 +17,16 @@ class MainFoodPage extends StatefulWidget {
 }
 
 class _MainFoodPageState extends State<MainFoodPage> {
+  Future<void> _loadResources() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return RefreshIndicator(
+      onRefresh: _loadResources,
+      child: Column(
         children: [
           /// showing the header
           Container(
