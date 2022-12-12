bbaed177f43bd6a35befbbf1fd62814d01644857
Mehedi <mehedi@augnitive.com>
2022-12-12 08:38:45 +0600

    profile screen layout given

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -4,6 +4,8 @@ import 'package:food_delivery/utils/dimension.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 
+import '../../widgets/account_widget.dart';
+
 class AccountPage extends StatelessWidget {
   const AccountPage({Key? key}) : super(key: key);
 
@@ -23,12 +25,86 @@ class AccountPage extends StatelessWidget {
         margin: EdgeInsets.only(top: Dimensions.height20),
         child: Column(
           children: [
+            /// profile
             AppIcon(
               icon: Icons.person,
               backgroundColor: AppColors.mainColor,
               iconColor: Colors.white,
               iconSize: Dimensions.height45 + Dimensions.height30,
               size: Dimensions.height15 * 10,
+            ),
+            SizedBox(height: Dimensions.height30),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    /// name
+                    AccountWidget(
+                      appIcon: AppIcon(
+                        icon: Icons.person,
+                        backgroundColor: AppColors.mainColor,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.iconSize24,
+                        size: Dimensions.height10 * 5,
+                      ),
+                      bigText: const BigText(text: 'Hasan'),
+                    ),
+                    SizedBox(height: Dimensions.height20),
+
+                    ///phone
+                    AccountWidget(
+                      appIcon: AppIcon(
+                        icon: Icons.phone,
+                        backgroundColor: AppColors.yellowColor,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.iconSize24,
+                        size: Dimensions.height10 * 5,
+                      ),
+                      bigText: const BigText(text: '0123456789'),
+                    ),
+                    SizedBox(height: Dimensions.height20),
+
+                    /// email
+                    AccountWidget(
+                      appIcon: AppIcon(
+                        icon: Icons.email,
+                        backgroundColor: AppColors.yellowColor,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.iconSize24,
+                        size: Dimensions.height10 * 5,
+                      ),
+                      bigText: const BigText(text: 'a@a.com'),
+                    ),
+                    SizedBox(height: Dimensions.height20),
+
+                    /// address
+                    AccountWidget(
+                      appIcon: AppIcon(
+                        icon: Icons.location_on,
+                        backgroundColor: AppColors.yellowColor,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.iconSize24,
+                        size: Dimensions.height10 * 5,
+                      ),
+                      bigText: const BigText(text: 'Fill in you address'),
+                    ),
+                    SizedBox(height: Dimensions.height20),
+
+                    /// message
+                    AccountWidget(
+                      appIcon: AppIcon(
+                        icon: Icons.message_outlined,
+                        backgroundColor: Colors.redAccent,
+                        iconColor: Colors.white,
+                        iconSize: Dimensions.iconSize24,
+                        size: Dimensions.height10 * 5,
+                      ),
+                      bigText: const BigText(text: 'Hasan'),
+                    ),
+                    SizedBox(height: Dimensions.height20),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
--- lib/widgets/account_widget.dart
+++ lib/widgets/account_widget.dart
@@ -21,9 +21,19 @@ class AccountWidget extends StatelessWidget {
         top: Dimensions.height10,
         bottom: Dimensions.height10,
       ),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 1,
+            offset: const Offset(0, 5),
+            color: Colors.grey.withOpacity(0.2),
+          ),
+        ],
+      ),
       child: Row(
         children: [
           appIcon,
+          SizedBox(width: Dimensions.width20),
           bigText,
         ],
       ),
