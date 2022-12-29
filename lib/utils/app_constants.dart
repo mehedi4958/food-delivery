c4fbc3c6b6ef82428cabd391440299a029153f78
Mehedi <mehedi@augnitive.com>
2022-12-29 15:46:11 +0600

    google map keys and configurations added

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   static const String APP_NAME = 'DBFood';
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = 'http://192.168.0.6:8000';
+  static const String BASE_URL = 'http://192.168.0.10:8000';
   static const String POPULAR_PRODUCT_URI = '/api/v1/products/popular';
   static const String RECOMMENDED_PRODUCT_URI = '/api/v1/products/recommended';
   // static const String DRINKS_URI = '/api/v1/products/drinks';
